fb6cc11eba3d5224e529b5a63f70b225b405ef73
rukiyeberna <rb.turan@hotmail.com>
2024-02-15 16:20:28 +0300

    Adding insertion sort algorithm implementation with pseudocode and example array

--- sorting_algorithms/insertion_sort.js
+++ sorting_algorithms/insertion_sort.js
@@ -8,10 +8,44 @@
 
     while(j<=0 && A[j] > temp )
     {
-        // bu kosulu sagladigi durumd
+        // bu kosulu sagladigi durumda shift yapiyoruz 
     }
 
 }*/
 
-function insertionSort(){}
-function printArray(){}
+
+let A = [5, 4, 10 ,1 ,6, 2, ];
+let arrSize = A.length;
+
+function insertionSort(arrSize,A)
+{
+    let i,j,temp;
+
+    for(i=1; i< arrSize; i++)
+    {
+        temp = A[i];
+        j= i-1;
+
+        while (j>=0 && A[j]> temp) 
+        {
+            A[j+1] = A[j];
+            j--;    
+        }
+        A[j+1] = temp;
+    }
+}
+function printArray(arrSize,A)
+{
+    let i;
+    for ( i=0; i<arrSize; i++) 
+    {
+        console.log(A[i] + "  "); 
+    }
+    console.log(" ");
+}
+
+ 
+// callback
+printArray(arrSize,A);
+insertionSort(arrSize,A);
+printArray(arrSize,A);
