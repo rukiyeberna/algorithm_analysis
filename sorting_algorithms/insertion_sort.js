50968d79bcfd845d85e1b09c2e87d6840dfff263
rukiyeberna <rb.turan@hotmail.com>
2024-02-15 13:36:41 +0300

    adding insertion sort file

---
+++ sorting_algorithms/insertion_sort.js
@@ -0,0 +1,17 @@
+/*elimizde 6 elemanli karisik siralanmis bir sayi dizisi olsun.--> n = 6*/
+/*insertion sort algoritmasi için araya yerleştirme algoritmasi diyebiliriz.*/
+/*------pseudocode--------*/
+/* for(i=1;i<n;i++)
+{
+    temp = A[i];
+    j=i-1;
+
+    while(j<=0 && A[j] > temp )
+    {
+        // bu kosulu sagladigi durumd
+    }
+
+}*/
+
+function insertionSort(){}
+function printArray(){}
